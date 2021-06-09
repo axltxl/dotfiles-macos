322c2ccde528ca33b7faa88687056a42048dd05b
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2021-06-09 16:21:29 +0200

    Alfred4 is no good to have it here

---
+++ alfred/reset-alfred4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ex
+
+rm -rf ~/Library/Application Support/Alfred
+rm -rf ~/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist
+rm -rf ~/Library/Preferences/com.alfredapp.Alfred.plist
+rm -rf ~/Library/Caches/com.runningwithcrayons.Alfred
